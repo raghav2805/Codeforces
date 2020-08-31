f4e42eaceaaa4e886d90c9d22b2eb8f6585ae9da
raghav2805 <59332635+raghav2805@users.noreply.github.com>
2020-08-31 16:14:37 +0530

    first question

---
+++ div-2_A.cpp
@@ -0,0 +1,45 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+
+        int arr[27]={0};
+
+        for(int i=0;i<n;i++){
+            string s;
+            cin>>s;
+
+            for(int j=0;j<s.length();j++){
+                arr[s[j]-'a']++;
+            }
+        }
+
+        bool flag=false;
+
+        for(int i=0;i<27;i++){
+            if(arr[i]!=0){
+                if(arr[i]%n!=0){
+                    flag=true;
+                }   
+            }
+        }
+
+        if(flag){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"Yes"<<endl;
+        }
+
+    }
+    return 0;
+
+}
