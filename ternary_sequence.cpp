b86abce6a3d25e780b7989bac2f095da31daec4d
raghav2805 <59332635+raghav2805@users.noreply.github.com>
2020-09-04 15:33:37 +0530

    good approach

---
+++ ternary_sequence.cpp
@@ -0,0 +1,36 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        
+        int m,smm=0,x0,x1,x2,y0,y1,y2;
+
+        cin>>x0>>x1>>x2;
+        cin>>y0>>y1>>y2;
+
+        m=min(x0,y2);
+        y2-=m;
+        x0-=m;
+
+        m=min(x1,y1);
+        x1-=m;
+        y0-=m;
+
+        m=min(x2,y1);
+        x2-=m;
+        y1-=m;
+        smm+=m*2;
+        
+        smm-=2*min(x1,y2);
+
+        cout<<smm<<endl;
+        
+    }
+    return 0;
+}
