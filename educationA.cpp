8ec5958fc2d475188da2a72b61fa0a713f40cf1e
raghav2805 <59332635+raghav2805@users.noreply.github.com>
2020-12-02 07:54:32 +0530

    Educational round 99 A

---
+++ educationA.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lli long long int
+ 
+ void solve(){
+    
+    string n;
+    cin>>n;
+
+    
+    cout<<n.size()<<endl;
+
+    return ;
+ }
+
+int main(){
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
